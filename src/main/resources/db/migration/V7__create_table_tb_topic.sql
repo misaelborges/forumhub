730e2e2c0e738d8d22bd01667f05b75cd24cb90c
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2024-12-24 19:10:41 -0300

    create V7__create_table_tb_topic.sql

--- src/main/resources/db/migration/V7__create_table_tb_topic.sql
+++ src/main/resources/db/migration/V7__create_table_tb_topic.sql
@@ -2,8 +2,8 @@ create table tb_topic (
     id bigint not null auto_increment,
     title varchar(255),
     message varchar(255),
-    creation_date datetime(6),
-    status enum ('CLOSED','NOT_ANSWERED','RESOLVED','UNRESOLVED'),
+    creation_date datetime,
+    topic_status enum ('CLOSED','NOT_ANSWERED','RESOLVED','UNRESOLVED'),
     user_id bigint,
     course_id bigint,
 
