27a961a21dd309436c540e836b7f64407b275445
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2024-12-24 19:15:37 -0300

    feat: implement Topic repository with custom queries

---
+++ src/main/java/com/misael/forumhub/domain/repositories/ITopicRepository.java
@@ -0,0 +1,23 @@
+package com.misael.forumhub.domain.repositories;
+
+import com.misael.forumhub.domain.models.Topic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ITopicRepository extends JpaRepository<Topic, Long> {
+
+    Page<Topic> findTop10ByOrderByCreationDateAsc(Pageable pageable);
+
+    @Query("""
+                SELECT t FROM Topic t 
+                JOIN t.course c
+                WHERE c.name = :nameCourse AND YEAR(t.creationDate) = :year
+            """)
+    List<Topic> findByCourseNameAndYear(String nameCourse, Integer year);
+}
