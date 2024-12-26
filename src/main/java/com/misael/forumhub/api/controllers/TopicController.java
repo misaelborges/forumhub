193433680e7fb1383af474e530318223d78080a6
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2024-12-26 17:04:04 -0300

    feat: add topic management endpoints
    - Add POST endpoint for creating topics
    - Add PUT endpoint for updating topics
    - Add DELETE endpoint for removing topics

--- src/main/java/com/misael/forumhub/api/controllers/TopicController.java
+++ src/main/java/com/misael/forumhub/api/controllers/TopicController.java
@@ -2,14 +2,19 @@ package com.misael.forumhub.api.controllers;
 
 import com.misael.forumhub.api.assemblers.TopicAssembler;
 import com.misael.forumhub.api.assemblers.TopicAssemblerDetail;
+import com.misael.forumhub.api.assemblers.TopicDisassembler;
+import com.misael.forumhub.api.dtos.TopicRequestDTO;
 import com.misael.forumhub.api.dtos.TopicResponseDTO;
 import com.misael.forumhub.api.dtos.TopicResponseDetailDTO;
-import com.misael.forumhub.domain.exceptions.NotFoundTopicException;
+import com.misael.forumhub.domain.models.Topic;
 import com.misael.forumhub.domain.repositories.ITopicRepository;
+import com.misael.forumhub.domain.services.TopicService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,6 +32,12 @@ public class TopicController {
     @Autowired
     private TopicAssemblerDetail topicAssemblerDetail;
 
+    @Autowired
+    private TopicDisassembler topicDisassembler;
+
+    @Autowired
+    private TopicService topicService;
+
     @GetMapping
     public Page<TopicResponseDTO> findAll(@PageableDefault(size = 10, sort = "title") Pageable pageable) {
         return topicAssembler.toCollectionModel(topicRepository.findTop10ByOrderByCreationDateAsc(pageable));
@@ -34,11 +45,34 @@ public class TopicController {
 
     @GetMapping("/{id}")
     public TopicResponseDetailDTO findById(@PathVariable Long id) {
-        return topicAssemblerDetail.toModel(topicRepository.findById(id).orElseThrow(() -> new NotFoundTopicException(id)));
+        return topicAssemblerDetail.toModel(topicService.findById(id));
     }
 
     @GetMapping("/search")
     List<TopicResponseDetailDTO>findByCourseNameAndYear(@RequestParam String nameCourse, @RequestParam Integer year) {
         return topicAssemblerDetail.toCollectionModel(topicRepository.findByCourseNameAndYear(nameCourse, year));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TopicResponseDTO save(@RequestBody @Valid TopicRequestDTO topicRequest) {
+        Topic topic = topicDisassembler.toDomainObject(topicRequest);
+
+        var topicSaved = topicService.save(topic);
+        return topicAssembler.toModel(topicSaved);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TopicResponseDTO update(@PathVariable Long id, @RequestBody @Valid TopicRequestDTO topicRequest) {
+        Topic topic = topicService.findById(id);
+        topicDisassembler.copyToDomainObject(topicRequest, topic);
+        topicService.save(topic);
+        return topicAssembler.toModel(topic);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        topicService.delete(id);
     }
 }
